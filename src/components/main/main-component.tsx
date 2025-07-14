6ae0fd70b4bd5dd4b9e4467bccdc2d458a768a6c
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-14 16:39:49 +0300

    feat: add basic layout to main-component and drafts for API functionality

--- src/components/main/main-component.tsx
+++ src/components/main/main-component.tsx
@@ -1,12 +1,29 @@
+// import { useState, useEffect } from 'react';
+import Results from './results-section/results-section.tsx';
 import './main-component.css';
 
 export default function Main() {
+  /*
+  const search = async (ev: MouseEvent) => {
+    if(ev.target instanceof HTMLInputElement) {
+      const val: string = ev.target.value;
+    };
+  };
+  */
+  const search = () => {};
   return (
     <main>
-      <section className="search-block">
-        <input type="search" className="search-field"></input>
-        <button className="search-btn">Search</button>
+      <section className="search-section">
+        <input
+          type="search"
+          className="search-field"
+          placeholder="Your input..."
+        ></input>
+        <button className="search-btn" onClick={search}>
+          Search
+        </button>
       </section>
+      <Results />
     </main>
   );
 }
