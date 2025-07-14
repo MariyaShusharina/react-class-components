840bad0f846a2dbf811ebed0128223bc0a00b74a
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-14 12:24:39 +0300

    feat: add basic components: header, main-component and footer

---
+++ src/components/main/main-component.tsx
@@ -0,0 +1,12 @@
+import './main-component.css';
+
+export default function Main() {
+  return (
+    <main>
+      <section className="search-block">
+        <input type="search" className="search-field"></input>
+        <button className="search-btn">Search</button>
+      </section>
+    </main>
+  );
+}
