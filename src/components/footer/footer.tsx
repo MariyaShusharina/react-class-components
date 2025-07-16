bdfce2c6e0f0bc52a7759040e5395b7491f08831
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-16 09:28:43 +0300

    fix: adjust header and footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,14 @@ import './footer.css';
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; 2025</p>
+      <p>2025</p>
+      <a
+        href="https://github.com/MariyaShusharina/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        MariyaShusharina
+      </a>
     </footer>
   );
 }
