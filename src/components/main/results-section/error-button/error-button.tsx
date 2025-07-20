86889b5691ea8d78c0ebaa8b0d667c25f90458b0
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-20 17:24:23 +0300

    refactor: add throwing of error to errorButton

--- src/components/main/results-section/error-button/error-button.tsx
+++ src/components/main/results-section/error-button/error-button.tsx
@@ -1,13 +1,22 @@
 import { Component } from 'react';
 
 export default class ErrorButton extends Component {
+  state = {
+    hasError: false,
+  };
+
   throwAnError() {
+    console.log(this.state.hasError);
+    this.setState({
+      hasError: true,
+    });
     throw Error('Test Error.');
   }
 
   render() {
+    console.log(this.state.hasError);
     return (
-      <div className="error-btn-container">
+      <div className="error-btn-container" key={this.state.hasError.toString()}>
         <button className="error-btn" onClick={this.throwAnError}>
           Throw an Error
         </button>
