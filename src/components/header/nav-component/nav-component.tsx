69c9bdd6ef5e27eea812cad4d7c77871816ba85c
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-28 11:57:25 +0300

    refactor: remove unnecessary condition in nav-component render

--- src/components/header/nav-component/nav-component.tsx
+++ src/components/header/nav-component/nav-component.tsx
@@ -6,24 +6,16 @@ export default function Nav() {
     <nav>
       <NavLink
         to="/"
-        className={({ isActive, isPending }) =>
-          isPending
-            ? 'nav-link pending'
-            : isActive
-              ? 'nav-link active'
-              : 'nav-link'
+        className={({ isActive }) =>
+          isActive ? 'nav-link active' : 'nav-link'
         }
       >
         Home
       </NavLink>
       <NavLink
         to="/about"
-        className={({ isActive, isPending }) =>
-          isPending
-            ? 'nav-link pending'
-            : isActive
-              ? 'nav-link active'
-              : 'nav-link'
+        className={({ isActive }) =>
+          isActive ? 'nav-link active' : 'nav-link'
         }
       >
         About
