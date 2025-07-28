6422fae142d84a14a339489f809734540016ca45
Mariya Shusharina <mariya.shushar@gmail.com>
2025-07-28 09:57:00 +0300

    feat: implement router and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import Footer from './components/footer/footer.tsx';
-import Header from './components/header/header.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/layout.tsx';
 import Home from './components/pages/home-page/home-page-component.tsx';
+import About from './components/pages/about-page/about-component.tsx';
+import Error404Page from './components/pages/error-404-page/error-404-component.tsx';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Error404Page />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
